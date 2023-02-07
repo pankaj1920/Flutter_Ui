24d8f4412a1013d8027bf821579088a7cd614db1
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-02-07 06:09:56 +0530

    Drop box ui completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,139 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String title = "hell";
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: HomeScreen(),
+        floatingActionButton: SizedBox(
+            width: 60,
+            height: 60,
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: kPurpleColor,
+              child: Icon(Icons.add),
+            )),
       ),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  List<Color> folderColors = [
+    kLightBlueColor,
+    kLightYellowColor,
+    kLightRedColor,
+    kLightGreenColor,
+    kLightBlueColor,
+    kLightYellowColor,
+    kLightRedColor,
+    kLightGreenColor,
+    kLightBlueColor,
+    kLightYellowColor,
+    kLightRedColor,
+    kLightGreenColor,
+    kLightBlueColor,
+    kLightYellowColor,
+    kLightRedColor,
+    kLightGreenColor,
+  ];
+
+  List<Color> textColors = [
+    kBlueColor,
+    kYellowColor,
+    kRedColor,
+    kGreenColor,
+    kBlueColor,
+    kYellowColor,
+    kRedColor,
+    kGreenColor,
+    kBlueColor,
+    kYellowColor,
+    kRedColor,
+    kGreenColor,
+    kBlueColor,
+    kYellowColor,
+    kRedColor,
+    kGreenColor,
+  ];
+
+  List<String> folderIconColors = [
+    'folder_icon_blue.svg',
+    'folder_icon_yellow.svg',
+    'folder_icon_red.svg',
+    'folder_icon_green.svg',
+    'folder_icon_blue.svg',
+    'folder_icon_yellow.svg',
+    'folder_icon_red.svg',
+    'folder_icon_green.svg',
+    'folder_icon_blue.svg',
+    'folder_icon_yellow.svg',
+    'folder_icon_red.svg',
+    'folder_icon_green.svg',
+    'folder_icon_blue.svg',
+    'folder_icon_yellow.svg',
+    'folder_icon_red.svg',
+    'folder_icon_green.svg',
+  ];
+
+  List<String> moreIconColors = [
+    'more_vertical_icon_blue.svg',
+    'more_vertical_icon_yellow.svg',
+    'more_vertical_icon_red.svg',
+    'more_vertical_icon_green.svg',
+    'more_vertical_icon_blue.svg',
+    'more_vertical_icon_yellow.svg',
+    'more_vertical_icon_red.svg',
+    'more_vertical_icon_green.svg',
+    'more_vertical_icon_blue.svg',
+    'more_vertical_icon_yellow.svg',
+    'more_vertical_icon_red.svg',
+    'more_vertical_icon_green.svg',
+    'more_vertical_icon_blue.svg',
+    'more_vertical_icon_yellow.svg',
+    'more_vertical_icon_red.svg',
+    'more_vertical_icon_green.svg',
+  ];
+
+  List<String> folderNames = [
+    'Mobile apps',
+    'SVG icons',
+    'Prototypes',
+    'Avatars',
+    'Mobile apps',
+    'SVG icons',
+    'Prototypes',
+    'Avatars',
+    'Mobile apps',
+    'SVG icons',
+    'Prototypes',
+    'Avatars',
+    'Mobile apps',
+    'SVG icons',
+    'Prototypes',
+    'Avatars',
+  ];
+
+  List<String> folderDates = [
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+    'December 20.2020',
+    'December 14.2020',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +161,7 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
-                "Your dribboxx",
+                "Your dribbox",
                 style: kQuestrialSemiBold.copyWith(
                     color: kBlackColor,
                     fontSize: SizeConfig.blockSizeHorizontal! * 6),
@@ -76,13 +198,73 @@ class HomeScreen extends StatelessWidget {
             height: SizeConfig.blockSzeVertical! * 4,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Recent'),
-              SizedBox(
-                width: SizeConfig.blockSizeHorizontal! * 2,
+              Row(
+                children: [
+                  Text('Recent',
+                      style: kQuestrialSemiBold.copyWith(
+                          fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                          color: kBlackColor)),
+                  SizedBox(
+                    width: SizeConfig.blockSizeHorizontal! * 2,
+                  ),
+                  SvgPicture.asset('assets/arrow_down_icon.svg')
+                ],
               ),
-              SvgPicture.asset('assets/arrow_down_icon.svg')
+              SvgPicture.asset('assets/sort_icon.svg')
             ],
+          ),
+          SizedBox(
+            height: SizeConfig.blockSzeVertical! * 3,
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: SizeConfig.blockSizeHorizontal! * 2,
+                mainAxisSpacing: SizeConfig.blockSizeHorizontal! * 2,
+                mainAxisExtent: 120),
+            itemCount: folderColors.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 0.5 * kPaddingHorizontal),
+                decoration: BoxDecoration(
+                    color: folderColors[index],
+                    borderRadius: BorderRadius.circular(kBorderRadius)),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SvgPicture.asset('assets/${folderIconColors[index]}'),
+                          SvgPicture.asset('assets/${moreIconColors[index]}'),
+                        ],
+                      ),
+                      SizedBox(
+                        height: SizeConfig.blockSzeVertical! * 1,
+                      ),
+                      Text(
+                        folderNames[index],
+                        style: kQuestrialSemiBold.copyWith(
+                            fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                            color: textColors[index]),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.blockSzeVertical! * .3,
+                      ),
+                      Text(
+                        folderDates[index],
+                        style: kQuestrialSemiBold.copyWith(
+                            fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
+                            color: textColors[index]),
+                      )
+                    ]),
+              );
+            },
           )
         ],
       ),

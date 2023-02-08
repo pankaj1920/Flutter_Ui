776f6eb864fb37d50bd0f3f2491703311de3543d
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-02-08 06:55:06 +0530

    Travel Ui

---
+++ lib/screens/travel_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TravelHomeScreen extends StatefulWidget {
+  const TravelHomeScreen({super.key});
+
+  @override
+  State<TravelHomeScreen> createState() => _TravelHomeScreenState();
+}
+
+class _TravelHomeScreenState extends State<TravelHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

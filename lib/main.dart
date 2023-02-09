7eb291a0574bec145e9e26a0323e2910351ef32e
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-02-09 06:57:57 +0530

    List view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dropbox_ui/app_style.dart';
 import 'package:dropbox_ui/dropbox_ui.dart';
+import 'package:dropbox_ui/screens/travel_home.dart';
 import 'package:dropbox_ui/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';

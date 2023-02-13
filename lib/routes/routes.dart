53b5f182e781ac8e49e2af7eb98c88c2e185a44f
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-02-13 07:27:59 +0530

    Route setup

---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+class MyRoute {
+  //CignifiRoutes
+  static String baseCignifiRoute = "/cignifi";
+  static String cignifiSplash = "$baseCignifiRoute/splash";
+  static String cignifiLogin = "$baseCignifiRoute/login";
+  static String cignifiSignUp = "$baseCignifiRoute/signup";
+}
---
+++ lib/ui_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UiList extends StatelessWidget {
+  const UiList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

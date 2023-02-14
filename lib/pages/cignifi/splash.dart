f99ffcd96118295659a129e50e5cc232720a6620
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-02-14 06:51:11 +0530

    splash screen completed

--- lib/pages/cignifi/splash.dart
+++ lib/pages/cignifi/splash.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_sample/app_style/colors.dart';
+import 'package:flutter_sample/routes/routes.dart';
 
-class CignifiSplash extends StatelessWidget {
+class CignifiSplash extends StatefulWidget {
   const CignifiSplash({super.key});
+
+  @override
+  State<CignifiSplash> createState() => _CignifiSplashState();
+}
+
+class _CignifiSplashState extends State<CignifiSplash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3),
+        () => {Navigator.pushNamed(context, MyRoute.cignifiLogin)});
+  }
 
   @override
   Widget build(BuildContext context) {
